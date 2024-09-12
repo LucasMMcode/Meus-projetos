bdfbb1ade013b9ff33bc6f06e7f6b440e1556754
LucasMMcode <144387466+LucasMMcode@users.noreply.github.com>
2024-09-12 12:20:30 -0300

    P
    p

--- Barbearia_Cris/produtos.php
+++ Barbearia_Cris/produtos.php
@@ -2,28 +2,47 @@
 session_start();?>
 <?php
 include "includes/cabecalho.inc"
+logado(){
+    echo  '<script>
+               '// window.location.href = "login.php";
+                .'alert("clicou")
+            </script>';
+}
 ?>
 
         <main>
             <ul class="produtos">
-                <a href="agendamento.php"><li>
+            <?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'<a href="agendamento.php">';}?><li>
                     <h2> Cabelo </h2>
                     <img src="img/cabelo.jpg">
                     <p class="Descrição"> Na tesoura ou máquina, como o cliente preferir </p>
                     <p class="Preço"> R$ 25,00 </p>
-                </li></a>
-                <a href="agendamento.php"><li>
+                </li><?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'</a>';}?>
+                <?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'<a href="agendamento.php">';}?><li>
                     <h2> Barba </h2>
                     <img src="img/barba.jpg">
                     <p class="Descrição"> Corte e desenhoo profisional de barba </p>
                     <p class="Preço"> R$ 18,00 </p>
-                </li></a>
-                <a href="agendamento.php"><li>
+                </li><?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'</a>';}?>
+                <?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'<a href="agendamento.php">';}?><li>
                     <h2> Cabelo + Barba </h2>
                     <img src="img/cabelo+barba.jpg">
                     <p class="Descrição"> Pacote comppleto de cabelo e barba </p>
                     <p class="Preço"> R$ 35,00 </p>
-                </li></a>
+                </li><?php
+                        if (isset($_SESSION['submit'])) {
+                            echo'</a>';}?>
+                            <button onclick="logado()">Agendar</button>
             </ul>
         </main>
 
