99d709f8d81fb79a5ce1c11ae1aaaa08e6dacebe
LucasMMcode <144387466+LucasMMcode@users.noreply.github.com>
2024-11-22 16:08:24 -0300

    a
    a

--- Barbearia_Cris/conexao.php
+++ Barbearia_Cris/conexao.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $user = "root";
     $pass = "";
-    //$pass = "Lucas_01lo";
+    $pass = "Lucas_01lo";
     $db_name = "bd_barbearia";  
     $conn = new mysqli($servername, $user, $pass, $db_name);
     if($conn->connect_error){
--- Barbearia_Cris/produtos.php
+++ Barbearia_Cris/produtos.php
@@ -12,7 +12,7 @@ include "includes/cabecalho.inc"
         if (isset($_SESSION['submit'])) {
         if($_SESSION['categoria']==1){
                             echo'<button class="cadastrap"><a href="cadastrap.php">Adicionar produtos</a></button>';
-                            echo "<form action='produtos.php' method='GET'>";}else{}}
+                            echo "<form action='produtos.php' method='POST'>";}else{}}
                             ?>
 
             <ul class="produtos" id="geraitens"><?php
@@ -22,25 +22,36 @@ include "includes/cabecalho.inc"
             $nome= array();
             $caminhoimg= array();
             $preco= array();
+
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {	
                 $idproduto[] = $row["IdProduto"];
                 $nome[] = $row["Nome"];
                 $preco[] = $row["Preco"];
                 $caminhoimg[] = $row["Caminhoimg"];
-                $existe[] = $row["Existe"];
                 }
             }
-            
+
             if (isset($_POST['submit'])) {
               for($i=0;$i<count($idproduto);$i++){
-                $postid[$i+1] = $_GET[$i+1];
+                $postid[$i+1] = $_POST[$i+1];
+                $idpost = $postid[$i+1];
+                $produtoid =$idproduto[$i];
+                $sql = "UPDATE `produtos` SET `Existe`=$idpost WHERE IdProduto = $produtoid";
+                $result = $conn->query($sql);
               }
             }
 
-            
+            $sql = "SELECT Existe FROM produtos";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+              while ($row = $result->fetch_assoc()) {	
+                $existe[] = $row["Existe"];
+                }
+            }
+
+
             for($i=0;$i<count($idproduto);$i++){
-              if (!isset($_POST['submit'])){ if($_GET[$i+1])echo $_GET[$i+1];}else{echo '0';}
               echo '<a><li><h2>'.' '.$nome[$i].' </h2>
               <img style="object-fit: cover;" src="img/'.$caminhoimg[$i].'"width=250px height=250px>
               <p class="Descrição"> '.$nome[$i].'</p>
@@ -48,14 +59,18 @@ include "includes/cabecalho.inc"
 
               if (isset($_SESSION['submit'])) {
                 if($_SESSION['categoria']==1){
-                  echo'<input type="checkbox" name="'.$idproduto[$i].'" value="1"'; if($existe[$i]){echo 'checked >';}else{echo'>';}
+                  echo'<p>Você deseja que este produto aparesa</p>';
+                  echo'<select name="'.$idproduto[$i].'">
+                    <option value="1"';if($existe[$i]) {echo 'selected';}echo'>Sim</option>
+                    <option value="0"';if(!$existe[$i]) {echo 'selected';}echo'>Não</option>
+                  </select>';
                 }
               }
               echo'</li></a>';
             }
             if (isset($_SESSION['submit'])) {
               if($_SESSION['categoria']==1){
-                echo '<input type="submit">';
+                echo '<input class="Submitp"type="submit" name="submit">';
                 echo "</form>";
               }
             }
